8784f60fc1473f202285486819ef7d85565f3e51
Samandar <zafarovsamandar5444@gmail.com>
2025-04-17 09:12:43 +0500

    finish

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -2,12 +2,13 @@ import React, { useState } from "react";
 import { YMaps, Map, Placemark } from "@iminside/react-yandex-maps";
 
 export default function About() {
+  const [loading, setLoading] = useState(false);
   const [langlot, setLanglot] = useState<{
     longitude: number;
     latitude: number;
   }>();
   const defaultState = {
-    center: [langlot?.latitude || 41 , langlot?.longitude || 69],
+    center: [langlot?.latitude || 41, langlot?.longitude || 69],
     zoom: 12,
   };
 
@@ -18,29 +19,29 @@ export default function About() {
         <button
           className="bg-blue-500 p-2"
           onClick={() => {
+            setLoading(true);
             navigator.geolocation.getCurrentPosition(
               (data) => {
                 setLanglot({
                   longitude: data.coords.longitude,
                   latitude: data.coords.latitude,
                 });
+                setLoading(false);
                 console.log(data);
               },
               (e) => {
                 console.log(e);
+                setLoading(false);
               }
             );
           }}
         >
-          meni Top
+          meni Top {loading&& "Loading"}
         </button>
       </div>
       <div>
         <YMaps>
-          <Map
-            style={{ width: "100%", height: 400 }}
-            state={defaultState}
-          >
+          <Map style={{ width: "100%", height: 400 }} state={defaultState}>
             {langlot && (
               <Placemark geometry={[langlot.latitude, langlot.longitude]} />
             )}
